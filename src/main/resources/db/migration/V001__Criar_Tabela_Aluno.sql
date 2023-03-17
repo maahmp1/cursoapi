a777688624c455270fbfa2de6aa5eb03b4add085
maahmp1 <marcelamartinspena@gmail.com>
2023-03-17 12:24:17 -0300

    add:migrations

--- src/main/resources/db/migration/V001__Criar_Tabela_Aluno.sql
+++ src/main/resources/db/migration/V001__Criar_Tabela_Aluno.sql
@@ -4,4 +4,3 @@ nomealuno varchar(60),
 id_curso int not null,
 id_cidade int not null
 );
-
--- src/main/resources/db/migration/V002__Criar_Tabela_Curso.sql
+++ src/main/resources/db/migration/V002__Criar_Tabela_Curso.sql
@@ -3,10 +3,6 @@ id int not null auto_increment primary key,
 nomecurso varchar (25)
 );
 
-Alter Table Curso add CONSTRAINT FK_Aluno_Curso foreign key(id_curso) references Curso(id);
-Alter Table Curso add CONSTRAINT FK_Aluno_Cidade foreign key(id_cidade) references Cidade(id);
- --colocar no cidade--
-
-insert into curso(nomecurso) values ('Análise e Desenvolvimento de Sistemas');
+insert into curso(nomecurso) values ('ADS');
 insert into curso(nomecurso) values ('Biologia');
 insert into curso(nomecurso) values ('Administração');
