6a8357e7f53cd415f3754c51aa93b6ec0c39b59b
maahmp1 <marcelamartinspena@gmail.com>
2023-05-25 08:54:38 -0300

    add:ending

--- src/main/java/br/com/etec/marcela/cursoapi/resources/CursoResources.java
+++ src/main/java/br/com/etec/marcela/cursoapi/resources/CursoResources.java
@@ -2,7 +2,10 @@ package br.com.etec.marcela.cursoapi.resources;
 
 import br.com.etec.marcela.cursoapi.model.Curso;
 import br.com.etec.marcela.cursoapi.repository.CursoRepository;
+import br.com.etec.marcela.cursoapi.repository.filter.CursoFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,11 @@ public class CursoResources {
 
     @Autowired
     private CursoRepository cursoRepository;
+
+    @GetMapping()
+    public Page<Curso> pesquisar(CursoFilter cursoFilter, Pageable pageable){
+        return cursoRepository.Filtrar(cursoFilter, pageable);
+    }
 
     @GetMapping("/todos")
     public List<Curso> listarTodosCursos() {
